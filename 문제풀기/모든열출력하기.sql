53c5bbe8a245215ca896611d6398e829e03d136c
junsu_Kim <96895747+junsu22@users.noreply.github.com>
2022-07-04 23:13:29 +0900

    Add files via upload

---
+++ 문제풀기/모든열출력하기.sql
@@ -0,0 +1,5 @@
+
+--사원 테이블 모든열(column) 전부 출력
+
+SELECT *
+    FROM emp;
---
+++ 문제풀기/컬럼명변경출력.sql
@@ -0,0 +1,5 @@
+-- 사원번호와 이름과 월급을 출력
+-- 컬럼명을 한글로 '사원번호','사원 이름' 으로 출력할것 
+
+SELECT empno AS 사원번호, ename AS 사원이름, sal AS "salary"
+	FROM emp;
