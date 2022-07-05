f85c800c5d5925d7b7fb128ac006644efaefe1fb
junsu_Kim <96895747+junsu22@users.noreply.github.com>
2022-07-05 23:40:19 +0900

    Add files via upload

---
+++ 문제풀기/where절1.sql
@@ -0,0 +1,4 @@
+-- 월급이 3000 이상인 사원들의 이름, 월급, 직업을 출력
+select  ename, sal, job
+from emp 
+where sal = 3000;
---
+++ 문제풀기/where절2.sql
@@ -0,0 +1,5 @@
+-- 문자와 날짜 
+-- 이름이 scott 인 사원의 이름,월급, 직업, 읿사일, 부서번호를 출력
+select ename, sal, job, hiredate, deptno
+from emp
+where ename='scott';
